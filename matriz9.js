7f7bd9188adff30960d19398563752bbfdc9a6d6
Herlyck <mrcyrax64@gmail.com>
2026-05-17 22:02:38 -0300

    Add files via upload

---
+++ matriz9.js
@@ -0,0 +1,20 @@
+const leer = require("prompt-sync")();
+
+
+function main() {
+    const matriz = [];
+
+    for (let i = 0; i < 3; i++) {
+        matriz.push([]);
+
+        for (let j = 0; j < 3; j++) {
+            matriz[i].push(j + 1 + (3 * i));
+        }
+
+
+    }
+    matriz[1].pop();
+    console.log(matriz);
+}
+
+main();
