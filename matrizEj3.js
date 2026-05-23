a978565517fc5e58964698bed357eca5a4725993
Herlyck <mrcyrax64@gmail.com>
2026-05-23 12:00:39 -0300

    Ejercicios de  Matrices versionado

--- matrizEj3.js
+++ matrizEj3.js
@@ -1,1 +1,13 @@
 const leer = require ("prompt-sync")();
+
+const DIAS=["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"];
+const TAREAS=[["lavar ropa","jugar con mascota"],[],["Ordenar cuarto","Cocinar","Ejercicio"],[],["Practicar Programacion"],[],[]]
+
+function main() {
+    let dia = 0;
+
+    console.log("Seleccione el dia a Modificar: 0 a 7");
+    dia = Number(leer());
+    
+}
+main();
