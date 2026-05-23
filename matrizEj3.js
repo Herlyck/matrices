af7f49e34968568e4d1571e6ec0e17d731d249d3
Herlyck <mrcyrax64@gmail.com>
2026-05-23 12:40:58 -0300

    ejercicio terminado

--- matrizEj3.js
+++ matrizEj3.js
@@ -5,9 +5,28 @@ const TAREAS=[["lavar ropa","jugar con mascota"],[],["Ordenar cuarto","Cocinar",
 
 function main() {
     let dia = 0;
+    let tareaEliminar=0;
 
     console.log("Seleccione el dia a Modificar: 0 a 7");
     dia = Number(leer());
     
+    console.log("***Mostrando Tareas del dia "+ DIAS[dia]+"***");
+
+    console.log(TAREAS[dia]); //esto tengo q repetir
+
+    console.log("que tarea desea eliminar?");
+    tareaEliminar=Number(leer());
+    TAREAS[dia].splice(tareaEliminar,1);
+
+    console.log("tarea/s restante/s:",DIAS[dia],TAREAS[dia]);
+    console.log(TAREAS);
+    
+
+    
+    
+    
+    
+
+
 }
 main();
