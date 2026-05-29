86fe06db562981f37279eb0e39d0245cf0ba05e1
Herlyck <mrcyrax64@gmail.com>
2026-05-29 19:38:42 -0300

    Hechizos Actualizado

--- hechizosMatriz.js
+++ hechizosMatriz.js
@@ -1,7 +1,36 @@
 const leer = require("prompt-sync")();
 
+const ESTUDIANTES = ["Harry", "Draco", "Luna"];
+const DIAS = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"];
+
+
 function main(params) {
-    
-    
+    const asistencia = [];
+    let asistenciaTemporal = 0;
+    let nombreEstudiante = 0;
+    let posEstudiante = 0;
+
+    for (let i = 0; i < ESTUDIANTES.length; i++) {
+        asistencia.push([]);
+        for (let j = 0; j < DIAS.length; j++) {
+            asistenciaTemporal = Math.floor(Math.random() * 2);
+            asistencia[i].push(asistenciaTemporal);
+
+        }
+    }
+
+    console.log("Consulta asistencia en Hogwarts\ningresa nombre del alumno:");
+    nombreEstudiante = leer();
+
+    posEstudiante = ESTUDIANTES.indexOf(nombreEstudiante);
+
+    console.log(asistencia[posEstudiante]);
+
+
+
+
+
+
+
 }
 main();
