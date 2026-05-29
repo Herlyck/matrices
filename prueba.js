709314c667c47bf368aed7ff0c0d3a3c463369db
Herlyck <mrcyrax64@gmail.com>
2026-05-29 18:36:32 -0300

    Hechizos

---
+++ prueba.js
@@ -0,0 +1,15 @@
+const leer = require("prompt-sync")();
+
+const DIAS = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"];
+const TAREAS = [["lavar ropa", "jugar con mascota"], [123], ["Ordenar cuarto", "Cocinar", "Ejercicio"], [], ["Practicar Programacion"], [], []];
+
+function main() {
+    for (let i = 0; i < DIAS.length; i++) {
+        if (TAREAS[i].length > 0) {
+            console.log(DIAS[i]+ " : "+ TAREAS[i]);
+        }
+    }
+
+
+}
+main();
